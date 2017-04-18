3dd0b07cc75b2ea9948ce3a1f4afac979d4893ca
Christian Martinez and Mark Fisher <christian@>
2017-04-18 14:48:03 -0700

    freeze mid-troubleshoot before massive refactor

--- js/AlarmClock-interface.js
+++ js/AlarmClock-interface.js
@@ -3,20 +3,31 @@
 
 var currentTime = null;
 var datetime = null;
-var userAlarmTime = null;
+userAlarmTime = moment().add(7, 'days');
 var alarmID;
 
 var update = function(){
   currentTime = moment();
   datetime.html(currentTime.format('h:mm:ss a'));
-  shouldAlarmGoOff(userAlarmTime)
-  console.log(userAlarmTime);
 }
-function alarmAlert(userAlarmTime) {
+
+function alarmAlert() {
   $.get('http://worldclockapi.com/api/json/utc/now', function(response){
-    console.log(response.currentDateTime);
-    var now = moment(response.currentDateTime);
-    alarmID = window.setTimeout(alert("wake up!"),(userAlarmTime.diff(now)));
+    // console.log(response.currentDateTime);
+    var now = moment(response.currentDateTime); //.format("HH:mm")
+    console.log(now);
+    console.log(userAlarmTime);
+    if(userAlarmTime.isBefore(now)){
+      alert("Wake up!");
+    }
+    // console.log(userAlarmTime.utc());
+    // var s = moment(userAlarmTime.diff(now, 'seconds'));
+    // console.log(s);
+    // console.log(s.i);
+    // console.log(s.i());
+    // console.log(s[1]);
+    // console.log(parseInt(s.format("ss")));
+    // alarmID = window.setTimeout(alert("wake up!"),moment(userAlarmTime.diff(now)));
   });
 }
 
@@ -32,12 +43,13 @@ $(document).ready(function() {
   $('#alarm-form').submit(function() {
     event.preventDefault();
     var userTime = $('#alarm-time').val();
-    console.log(typeof userTime);
-    userAlarmTime = moment(userTime,'YYYY-MM-DDTHH:mmZ');
-    // userAlarmTime = moment(userTime,'MM:HH');
-    console.log(typeof userAlarmTime);
-    console.log(userAlarmTime);
+    // console.log(typeof userTime);
+    userAlarmTime = moment(userTime,'HH:mm').utc();
     alarmAlert(userAlarmTime);
+    // userAlarmTime = moment(userTime,'MM:HH');
+    // console.log(typeof userAlarmTime);
+    // console.log(userAlarmTime);
     $('#times').append('<li>' + userTime + '</li>');
   });
+  // setInterval(alarmAlert, 800000);
 });
