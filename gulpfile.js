70c4b75091212d76aef5888cb2ba0706bb308341
Christian Martinez and Mark Fisher <mark@>
2017-04-18 16:46:39 -0700

    add a few more things for sass

--- gulpfile.js
+++ gulpfile.js
@@ -109,5 +109,6 @@ gulp.task('cssBuild', function() {
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
