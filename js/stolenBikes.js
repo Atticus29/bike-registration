fe71436edf26e591023c2054558c378cece598a2
Mark <mark.aaron.fisher@gmail.com>
2017-04-19 16:05:21 -0700

    fix the issue with the bike array not returning

--- js/stolenBikes.js
+++ js/stolenBikes.js
@@ -12,16 +12,19 @@ Bike = function(dateStolen, frameColors, frameModel, id, imageUrl, manufacturer,
 };
 
 Bike.prototype.addStolenBikesToArray = function(allBikes){
-  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=9421%20North%20Portsmouth%20Ave%2C%20Portland%2C%20OR&distance=10&stolenness=stolen")
-  .then(function(response){
+  $.get("https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen")
+  .then((response) => {
     response.bikes.forEach(function(bike){
       var currentBike = new Bike(bike.date_stolen, bike.frame_colors, bike.frame_model, bike.id, bike.thumb, bike.manufacturer_name, bike.serial, bike.stolen_location, bike.title, bike.year);
       allBikes.push(currentBike);
       // this displays an ever-growing array of bike objects
-      console.log(allBikes);
-    });
+      // console.log(allBikes);
+    })
   })
-  .then(console.log(allBikes));
+  // this displays an empty array
+  .then(() => {
+    console.log(allBikes)
+  });
 };
 
 Bike.prototype.setColor = function (userName){
